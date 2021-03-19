aa6c93b96a16382df609ef493a27d1cfdca3a2c3
Kevin Stiehl <kevin.stiehl@numericas.de>
2021-03-19 17:01:08 +0100

    Allow download from private repositories

--- src/downloaders/github-downloader.ts
+++ src/downloaders/github-downloader.ts
@@ -1,19 +1,31 @@
 import * as core from '@actions/core'
-import * as http from '@actions/http-client'
-import {HttpDownloadBuilder} from './http-downloader'
+import {Headers, HttpDownloadBuilder} from './http-downloader'
 
+export interface Release {
+  id: string
+  tag_name: string
+  assets: {
+    name: string
+    url: string
+  }[]
+}
 
 export class GithubDownloaderBuilder extends HttpDownloadBuilder {
   protected _githubEndpoint = 'https://github.com'
   protected _githubOrg: string
   protected _githubApiEndpoint?: string
 
-  private readonly _githubRepository: string;
+  private readonly _githubRepository: string
 
   constructor(githubOrg: string, githubRepository: string, name: string, version: string) {
     super(name, version, '')
     this._githubOrg = githubOrg
     this._githubRepository = githubRepository
+  }
+
+  auth(username?: string, password?: string): GithubDownloaderBuilder {
+    super.auth(username, password)
+    return this
   }
 
   public githubEndpoint(githubEndpoint: string): GithubDownloaderBuilder {
@@ -26,35 +38,45 @@ export class GithubDownloaderBuilder extends HttpDownloadBuilder {
     return this
   }
 
-  public async download(): Promise<string> {
+  async download(headers: Headers = {}): Promise<string> {
     await this.refreshUrl()
-    return super.download()
+    return super.download({...headers, 'accept': 'application/octet-stream'})
   }
 
   private async refreshUrl() {
-    const commonUrlPrefix = `${this._githubEndpoint}/${this._githubOrg}/${this._githubRepository}/releases`
+    let release = null
     if (this._version === 'latest' || this._version === '') {
-      this._version = await this.getLatestRelease()
-      core.info('Downloading latest release of pipsaper')
-      this._url = `${commonUrlPrefix}/latest/download/${this._name}`
+      release = await this.getLatestRelease()
+      core.info('Downloading latest release of piper')
+    } else if (this._version) {
+      release = await this.getReleaseByTag(this._version)
     } else {
-      core.info(`Downloading tag ${this._version} of piper`)
-      this._url = `${commonUrlPrefix}/download/${this._version}/${this._name}`
+      throw new Error('no version specified')
     }
+    this._version = release.tag_name
+    const assetUrl = release.assets.filter(asset => asset.name == this._name).map(asset => asset.url)[0]
+    core.debug(`Downloading from ${assetUrl}`)
+    this._url = assetUrl
   }
 
-  private async getLatestRelease(): Promise<string> {
-    core.debug('Looking up latest version')
-    let headers = {}
-    if (this._auth) {
-      headers = {...headers, Authorization: this._auth}
-    }
-    const client = new http.HttpClient('sap-piper-action', [], headers)
+  private async getReleaseByTag(tagName: string): Promise<Release> {
+    core.debug('Loading release for tags')
+    const client = this.getHttpClient({accept: 'application/json'})
+    const requestUrl = `${this.getApiEndpoint()}/repos/${this._githubOrg}/${this._githubRepository}/releases/tags/${tagName}`
+    const res = await client.get(requestUrl)
+    const release = await res.data
+    core.debug(`latest release was resolved to ${release.tag_name}`)
+    return release
+  }
+
+  private async getLatestRelease(): Promise<Release> {
+    core.debug('Looking up latest release')
+    const client = this.getHttpClient({accept: 'application/json'})
     const requestUrl = `${this.getApiEndpoint()}/repos/${this._githubOrg}/${this._githubRepository}/releases/latest`
     const res = await client.get(requestUrl)
-    const {tag_name} = JSON.parse(await res.readBody())
-    core.debug(`latest version was resolved to ${tag_name}`)
-    return tag_name
+    const release = await res.data
+    core.debug(`latest release was resolved to ${release.tag_name}`)
+    return release
   }
 
   private getApiEndpoint(): string {
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-import {GithubDownloaderBuilder} from './downloaders'
-
 export * from './downloaders'
 export * from './utils'
 export * from './output-generators'
-
-
-new GithubDownloaderBuilder('SAP', 'jenkins-library', 'piper', 'latest').download()
